1a58f63fbb2fe9624474dbce43f1c6c46ed97089
Sean Aguinaga <aguinaga.sean@gmail.com>
2024-10-04 08:35:35 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Offlinez',
+  webDir: 'dist'
+};
+
+export default config;
